70c742ab6529e6e94f2e7e7c5490a1067913c79f
LuisGustavo07 <luisgustavovascosta@gmail.com>
2025-04-10 09:06:29 -0300

    Variável nome e tipo do vinho
    Variáveis de nome e tipo vinho + alert caso não envie informações

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+var nome_vinho= prompt("Digite o nome do vinho: ");
+if (nome_vinho==""){
+    alert("Nome do vinho não inserido");
+    var nome_vinho= prompt("Digite o nome do vinho: ");
+}
+
+var tipo_vinho= prompt("Digite o tipo do vinho: ");
+if (tipo_vinho==""){
+    alert("Tipo do vinho não inserido");
+    var tipo_vinho= prompt("Digite o nome do vinho: ");
+}
