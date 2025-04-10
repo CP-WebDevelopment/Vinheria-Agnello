62d9e50f20ce9b7965546ed4828ac3a1873304d2
Anna Ruggeri <ruggeriannaclara@gmail.com>
2025-04-10 09:27:51 -0300

    IF de estoque baixo
    IF de estoque baixo mais console.log nas informações

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ if (nome_vinho==""){
     alert("Nome do vinho não inserido");
     var nome_vinho= prompt("Digite o nome do vinho: ");
 }
+console.log("Nome do vinho: ", nome_vinho)
 
 //TIPO DO VINHO
 var tipo_vinho= prompt("Digite o tipo do vinho: ");
@@ -11,6 +12,7 @@ if (tipo_vinho==""){
     alert("Tipo do vinho não inserido");
     var tipo_vinho= prompt("Digite o tipo do vinho: ");
 }
+console.log("Tipo do vinho: ", tipo_vinho)
 
 //SAFRA DO VINHO
 var safra_vinho= prompt("Digite o Safra do vinho: ");
@@ -18,6 +20,7 @@ if (safra_vinho==""){
     alert("Safra do vinho não inserida");
     var safra_vinho= prompt("Digite a safra do vinho: ");
 }
+console.log("Safra do vinho: ", safra_vinho)
 
 //QUANTIDADE DO VINHO
 var qtd_vinho= prompt("Digite a quantidade do vinho: ");
@@ -25,3 +28,8 @@ if (qtd_vinho==""){
     alert("Quantidade do vinho não inserida");
     var qtd_vinho= prompt("Digite a quantidade do vinho: ");
 }
+if (qtd_vinho<5){ //SE FOR MENOR QUE 5 EXIBIR O ALERT
+    alert("Estoque baixo")
+}
+console.log("Estoque do vinho: ", qtd_vinho)
+alert("Cadastro realizado! Veja as informações no console.")
