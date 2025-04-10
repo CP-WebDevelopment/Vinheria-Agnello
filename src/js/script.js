5f3d8a7005f9f3685ca1240f64ef57a9a0b7d384
Giovana Carnevali <giovanacarnevali@outlook.com>
2025-04-10 09:15:56 -0300

    Variável safra e quantidade do vinho
    Variável safra e quantidade do vinho + alert caso não envie informações

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,27 @@
+//NOME DO VINHO
 var nome_vinho= prompt("Digite o nome do vinho: ");
 if (nome_vinho==""){
     alert("Nome do vinho não inserido");
     var nome_vinho= prompt("Digite o nome do vinho: ");
 }
 
+//TIPO DO VINHO
 var tipo_vinho= prompt("Digite o tipo do vinho: ");
 if (tipo_vinho==""){
     alert("Tipo do vinho não inserido");
-    var tipo_vinho= prompt("Digite o nome do vinho: ");
+    var tipo_vinho= prompt("Digite o tipo do vinho: ");
+}
+
+//SAFRA DO VINHO
+var safra_vinho= prompt("Digite o Safra do vinho: ");
+if (safra_vinho==""){
+    alert("Safra do vinho não inserida");
+    var safra_vinho= prompt("Digite a safra do vinho: ");
+}
+
+//QUANTIDADE DO VINHO
+var qtd_vinho= prompt("Digite a quantidade do vinho: ");
+if (qtd_vinho==""){
+    alert("Quantidade do vinho não inserida");
+    var qtd_vinho= prompt("Digite a quantidade do vinho: ");
 }
